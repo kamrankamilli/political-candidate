b1a094f772c51219e5f45cbf05b6a4fd19647494
Kamran Kamilli <kamilli818@gmail.com>
2023-01-06 17:42:08 +0300

    image optimization

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import style from "./Logo.module.css";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.webp";
 const Logo = () => {
   const clickHandler = () => {
     document.documentElement.scrollTo({
