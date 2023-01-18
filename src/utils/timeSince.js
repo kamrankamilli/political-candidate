9f308ec2c65f808d407b15fc58b0a8e26b45ee03
Kamran Kamilli <kamilli818@gmail.com>
2023-01-18 18:09:39 +0300

    timeSince changed

--- src/utils/timeSince.js
+++ src/utils/timeSince.js
@@ -4,24 +4,24 @@ function timeSince(date) {
   let interval = seconds / 31536000;
 
   if (interval > 1) {
-    return Math.floor(interval) + " years";
+    return Math.floor(interval) + " yıl";
   }
   interval = seconds / 2592000;
   if (interval > 1) {
-    return Math.floor(interval) + " months";
+    return Math.floor(interval) + " ay";
   }
   interval = seconds / 86400;
   if (interval > 1) {
-    return Math.floor(interval) + " days";
+    return Math.floor(interval) + " gün";
   }
   interval = seconds / 3600;
   if (interval > 1) {
-    return Math.floor(interval) + " hours";
+    return Math.floor(interval) + " saat";
   }
   interval = seconds / 60;
   if (interval > 1) {
-    return Math.floor(interval) + " minutes";
+    return Math.floor(interval) + " dakika";
   }
-  return Math.floor(seconds) + " seconds";
+  return Math.floor(seconds) + " saniye";
 }
 export default timeSince;
