e30ac9873be3646c951e57f2a574252f0af7071b
Kamran Kamilli <kamilli818@gmail.com>
2023-01-18 18:07:29 +0300

    twitter content added

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,23 +4,23 @@ import TwitList from "../components/Twits/TwitList";
 const twits = [
   {
     id: "1",
-    text: '" Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, numquam. Aliquam dolor vitae architecto officiis aliquid veritatis doloremque, consequatur harum nostrum reprehenderit animi odit enim voluptates! Nisi exercitationem voluptates ullam? "',
-    createdAt: "Mon Dec 18 00:13:42 +0300 2022",
+    text: '" Analarımız, bacılarımız, hayatımızın yarısı hatta çok daha fazla değerlerimizi ifade eden kadınlarımızın Kadın Hakları Gününü kutluyorum "',
+    createdAt: "Mon Dec 5 00:00:00 +0300 2022",
   },
   {
     id: "2",
-    text: '" Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, numquam. Aliquam dolor vitae architecto officiis aliquid veritatis doloremque, consequatur harum nostrum reprehenderit animi odit enim voluptates! Nisi exercitationem voluptates ullam? "',
-    createdAt: "Mon Dec 15 00:13:42 +0300 2022",
+    text: '"  Şimdiye kadar uygulanan eğitim ve öğretim yöntemlerinin milletimizin geri kalmasında en önemli etken olduğu kanısındayım. Onun için bir milli eğitim programından söz ederken, eski devrin boş inançlarından ve yaratılışımızla hiç ilgisi olmayan yabancı fikirlerden, doğudan ve batıdan gelebilen bütün etkilerden tamamen uzak, milli karakterimiz ve milli tarihimizle uyumlu bir kültür kastediyorum. Çünkü milli dehamızın gelişmesi ancak böyle bir kültür ile sağlanabilir. "',
+    createdAt: "Mon Oct 15 00:13:42 +0300 2022",
   },
   {
     id: "3",
-    text: '" Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, numquam. Aliquam dolor vitae architecto officiis aliquid veritatis doloremque, consequatur harum nostrum reprehenderit animi odit enim voluptates! Nisi exercitationem voluptates ullam? "',
-    createdAt: "Mon Dec 15 00:13:42 +0300 2022",
+    text: '" Gümrük vergilerini ortadan kaldıralım ve böylece halkların ittifakı ilan edilecek, dayanışmaları tanınacak ve eşitlikleri ilan edilecektir. "',
+    createdAt: "Mon Sep 25 00:13:42 +0300 2022",
   },
   {
     id: "4",
-    text: '" Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, numquam. Aliquam dolor vitae architecto officiis aliquid veritatis doloremque, consequatur harum nostrum reprehenderit animi odit enim voluptates! Nisi exercitationem voluptates ullam? "',
-    createdAt: "Mon Dec 15 00:13:42 +0300 2022",
+    text: '" Bugün, Türkiye’nin karşı karşıya bulunduğu en önemli sorunlardan biri, kadın ve erkek arasındaki yasal ve toplumsal eşitliği sağlamaktır. Kadının zorla köşeye çekilmesinin aile yaşamını mahvettiği yerde hiçbir ilerleme mümkün değildir "',
+    createdAt: "Mon Aug 19 00:13:42 +0300 2022",
   },
 ];
 const LandingPage = () => {
